a1f38cbf71e3dcce8a80fa0761872463319b798c
lucasjunior24 <lukateku@gmail.com>
2022-01-15 13:05:25 -0300

    Utilizando proporçOes em medidas

--- src/screens/Dashboard/styles.ts
+++ src/screens/Dashboard/styles.ts
@@ -1,16 +1,52 @@
+import { RFPercentage, RFValue } from 'react-native-responsive-fontsize';
 import styled from 'styled-components/native';
 
 export const Container = styled.View`
     flex: 1;
-    justify-content: center;
-    align-items: center;
-
     background: ${({ theme }) => theme.colors.background };
 `;
 
-export const Title = styled.Text`
+export const Header = styled.View`
+    width: 100%;
+    height: ${RFPercentage(42)}px;
+    justify-content: center;
+    align-items: center;
+    flex-direction: row;
+
+    background: ${({ theme }) => theme.colors.primary };
+`;
+
+export const UserWrapper = styled.View`
+    width: 100%;
+    padding: 0 24px;
+`;
+
+export const UserInfo = styled.View`
+    align-items: center;
+    flex-direction: row;
+`;
+
+export const Photo = styled.Image`
+    width: ${RFValue(55)}px;
+    height: ${RFValue(55)}px;
+
+    border-radius: 10px;
+`;
+
+export const User = styled.View`
+    margin-left: 17px;
+`;
+
+export const UserGreeting = styled.Text`
+    color: ${({ theme }) => theme.colors.shape };
+
+    font-size: ${RFValue(18)}px;
+    font-family: ${({ theme }) => theme.fonts.regular };
+`;
+
+export const UserName = styled.Text`
+    color: ${({ theme }) => theme.colors.shape };
+
+    font-size: ${RFValue(18)}px;
     font-family: ${({ theme }) => theme.fonts.bold };
-    font-size: 24px;
-    color: ${({ theme }) => theme.colors.title };
-    font-weight: bold;
 `;
